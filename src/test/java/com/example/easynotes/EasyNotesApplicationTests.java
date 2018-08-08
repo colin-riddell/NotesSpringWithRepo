c9c135ea894112fd97eb4245a309d1b91f3cf058
Colin Riddell <colin.riddell@codeclan.com>
2018-08-08 14:51:53 +0100

    Adding first rest mvc test

--- src/test/java/com/example/easynotes/EasyNotesApplicationTests.java
+++ src/test/java/com/example/easynotes/EasyNotesApplicationTests.java
@@ -2,30 +2,63 @@ package com.example.easynotes;
 
 import com.example.easynotes.model.Note;
 import com.example.easynotes.repository.NoteRepository;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
-import java.util.List;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+import java.nio.charset.Charset;
 
-import static org.junit.Assert.assertEquals;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class EasyNotesApplicationTests {
 
+	private MockMvc mockMvc;
+
+	private Note note;
+
+	private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+			MediaType.APPLICATION_JSON.getSubtype(),
+			Charset.forName("utf8"));
+
 	@Autowired // Inject a version of the NoteRepository here
 	NoteRepository noteRepository;
 
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+	@Before
+	public void setup(){
+		this.mockMvc = webAppContextSetup(webApplicationContext).build();
+
+		note = new Note("1234a", "blah blah blah");
+		noteRepository.save(note);
+	}
+
 	@Test
 	public void contextLoads() {
-		Note note = new Note("1234a", "blah blah blah");
-		noteRepository.save(note);
+		//TODO: Find out what this was for
+	}
 
-        List<Note> foundNotesByTitle =  noteRepository.noteByTitle("1234a");
-        assertEquals(note.getTitle(), "1234a");
-
+	@Test
+	public void testGetNotesWithTitle() throws Exception{
+		mockMvc.perform(get("/api/notes/1234a"))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(contentType))
+				.andExpect( jsonPath("$[0].title").value("1234a"));
 	}
 
 }
